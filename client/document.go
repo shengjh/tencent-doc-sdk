2db9bc12f047700fd41cf661310d1fa8f07130ff
hello w! <43599939@qq.com>
2025-04-23 15:35:01 +0800

    feat: 完善文档

--- client/document.go
+++ client/document.go
@@ -104,6 +104,16 @@ func (c *Client) SearchDocuments(ctx context.Context, params *model.SearchParams
 	q.Add("byOwnership", fmt.Sprintf("%d", params.ByOwnership))
 	q.Add("offset", fmt.Sprintf("%d", params.Offset))
 	q.Add("size", fmt.Sprintf("%d", params.Size))
+	if params.FileTypes != "" {
+		q.Add("fileTypes", params.FileTypes)
+	}
+	if params.SortType != "" {
+		q.Add("sortType", params.SortType)
+	}
+	if params.Asc != 0 {
+		q.Add("asc", fmt.Sprintf("%d", params.Asc))
+	}
+
 	u.RawQuery = q.Encode()
 
 	// 发送请求
@@ -121,6 +131,46 @@ func (c *Client) SearchDocuments(ctx context.Context, params *model.SearchParams
 	)
 	if err != nil {
 		return nil, fmt.Errorf("search documents failed: %w", err)
+	}
+
+	if result.Ret != 0 {
+		return nil, fmt.Errorf("api error: %s (ret=%d)", result.Msg, result.Ret)
+	}
+
+	return &result, nil
+}
+
+// GetFileMetadata 获取文件元数据
+/*
+  指引地址：https://docs.qq.com/open/document/app/openapi/v2/file/files/metadata.html
+*/
+func (c *Client) GetFileMetadata(ctx context.Context, fileID string) (*model.FileMetadataResponse, error) {
+	if c.token == nil || c.token.AccessToken == "" {
+		return nil, fmt.Errorf("access token is required")
+	}
+
+	// 构建请求URL
+	endpoint := fmt.Sprintf("%s/drive/v2/files/%s/metadata", constant.APIEndpoint, fileID)
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("parse endpoint failed: %w", err)
+	}
+
+	// 发送请求
+	var result model.FileMetadataResponse
+	err = util.GetWithCustomHeaders(
+		ctx,
+		c.httpClient,
+		u.String(),
+		map[string]string{
+			"Access-Token": c.token.AccessToken,
+			"Client-Id":    c.config.ClientID,
+			"Open-Id":      c.token.UserID,
+		},
+		&result,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("get file metadata failed: %w", err)
 	}
 
 	if result.Ret != 0 {
--- model/document.go
+++ model/document.go
@@ -71,10 +71,35 @@ type SearchDocumentsResponse struct {
 
 // SearchParams 搜索参数
 type SearchParams struct {
-	SearchType  string `json:"searchType"`  // title/content
-	SearchKey   string `json:"searchKey"`   // 搜索关键词
-	FolderID    string `json:"folderID"`    // 文件夹ID
-	ByOwnership int    `json:"byOwnership"` // 按所有权过滤
-	Offset      int    `json:"offset"`      // 偏移量
-	Size        int    `json:"size"`        // 每页大小
+	SearchKey   string `json:"searchKey"`   // 搜索关键字(必填)
+	SearchType  string `json:"searchType"`  // 搜索类型(必填): title-标题搜索 owner-所有者搜索
+	ResultType  string `json:"resultType"`  // 返回结果类型: all-所有 folder-只返回文件夹(默认all)
+	FolderID    string `json:"folderID"`    // 搜索范围文件夹ID，空表示所有文件
+	Offset      int    `json:"offset"`      // 起始偏移量(默认0)
+	Size        int    `json:"size"`        // 返回条目数量(默认20，最大50)
+	SortType    string `json:"sortType"`    // 排序规则: modify-修改时间(默认) create-创建时间 browse-访问时间
+	Asc         int    `json:"asc"`         // 排序顺序: 1-正序 0-倒序(默认)
+	ByOwnership int    `json:"byOwnership"` // 所有者过滤: 1-是所有者 0-否(默认)
+	FileTypes   string `json:"fileTypes"`   // 文件类型过滤，多种用"-"分隔，如"doc-sheet"
+}
+
+// FileMetadataResponse 文件元数据响应结构
+type FileMetadataResponse struct {
+	Ret  int    `json:"ret"` // 返回码
+	Msg  string `json:"msg"` // 返回信息
+	Data struct {
+		ID             string `json:"ID"`             // 文件唯一标识
+		Title          string `json:"title"`          // 文件标题
+		Type           string `json:"type"`           // 文件类型(doc/sheet/ppt等)
+		URL            string `json:"url"`            // 文件访问URL
+		Status         string `json:"status"`         // 文件状态(normal/deleted等)
+		IsCreator      bool   `json:"isCreator"`      // 当前用户是否为创建者
+		CreateTime     int64  `json:"createTime"`     // 创建时间戳(秒级)
+		CreatorName    string `json:"creatorName"`    // 创建者名称
+		IsOwner        bool   `json:"isOwner"`        // 当前用户是否为所有者
+		OwnerName      string `json:"ownerName"`      // 所有者名称
+		LastModifyTime int64  `json:"lastModifyTime"` // 最后修改时间戳(秒级)
+		LastModifyName string `json:"lastModifyName"` // 最后修改者名称
+		OwnerID        string `json:"ownerID"`        // 所有者ID
+	} `json:"data"` // 文件元数据详情
 }
